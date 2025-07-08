232c6b5866983c26438698a7f3662c95bce481f4
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2025-07-08 15:45:28 -0700

    Changed the local db setup

--- server/index.ts
+++ server/index.ts
@@ -68,8 +68,8 @@ import { seedMongoArticlesIfEmpty } from "./storage";
   server.listen(
     {
       port,
-      host: "0.0.0.0",
-      reusePort: true,
+      host: "127.0.0.1",
+      // reusePort: true,
     },
     () => {
       log(`serving on port ${port}`);
